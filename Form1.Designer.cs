855cc7be712e8ac962cebf7583f6cfe2698c7009
asheryy <24521557@gm.uit.edu.vn>
2025-09-26 23:22:18 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace baitap1
+{
+    partial class frmĐN
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmĐN));
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtDN = new System.Windows.Forms.TextBox();
+            this.txtMK = new System.Windows.Forms.TextBox();
+            this.btnĐK = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.chkHide = new System.Windows.Forms.CheckBox();
+            this.picHide = new System.Windows.Forms.PictureBox();
+            this.picShow = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picHide)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picShow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(288, 287);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(125, 56);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Đăng Nhập";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtDN
+            // 
+            this.txtDN.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDN.Location = new System.Drawing.Point(233, 109);
+            this.txtDN.Margin = new System.Windows.Forms.Padding(2);
+            this.txtDN.Name = "txtDN";
+            this.txtDN.Size = new System.Drawing.Size(240, 27);
+            this.txtDN.TabIndex = 3;
+            this.txtDN.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtMK
+            // 
+            this.txtMK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMK.Location = new System.Drawing.Point(233, 192);
+            this.txtMK.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMK.Name = "txtMK";
+            this.txtMK.Size = new System.Drawing.Size(240, 27);
+            this.txtMK.TabIndex = 4;
+            this.txtMK.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtMK.TextChanged += new System.EventHandler(this.txtMK_TextChanged);
+            // 
+            // btnĐK
+            // 
+            this.btnĐK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnĐK.Location = new System.Drawing.Point(350, 386);
+            this.btnĐK.Name = "btnĐK";
+            this.btnĐK.Size = new System.Drawing.Size(174, 34);
+            this.btnĐK.TabIndex = 5;
+            this.btnĐK.Text = "Đăng kí tại đây";
+            this.btnĐK.UseVisualStyleBackColor = true;
+            this.btnĐK.Click += new System.EventHandler(this.btnĐK_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.BackColor = System.Drawing.Color.Transparent;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(129, 393);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(215, 23);
+            this.lbl1.TabIndex = 6;
+            this.lbl1.Text = "Nếu không có tài khoản";
+            // 
+            // chkHide
+            // 
+            this.chkHide.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.chkHide.Location = new System.Drawing.Point(488, 202);
+            this.chkHide.Name = "chkHide";
+            this.chkHide.Size = new System.Drawing.Size(15, 17);
+            this.chkHide.TabIndex = 7;
+            this.chkHide.UseVisualStyleBackColor = true;
+            this.chkHide.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // picHide
+            // 
+            this.picHide.Image = ((System.Drawing.Image)(resources.GetObject("picHide.Image")));
+            this.picHide.Location = new System.Drawing.Point(519, 183);
+            this.picHide.Name = "picHide";
+            this.picHide.Size = new System.Drawing.Size(42, 36);
+            this.picHide.TabIndex = 8;
+            this.picHide.TabStop = false;
+            // 
+            // picShow
+            // 
+            this.picShow.Image = ((System.Drawing.Image)(resources.GetObject("picShow.Image")));
+            this.picShow.Location = new System.Drawing.Point(519, 183);
+            this.picShow.Name = "picShow";
+            this.picShow.Size = new System.Drawing.Size(42, 36);
+            this.picShow.TabIndex = 9;
+            this.picShow.TabStop = false;
+            this.picShow.Click += new System.EventHandler(this.picShow_Click);
+            // 
+            // frmĐN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(767, 538);
+            this.Controls.Add(this.picShow);
+            this.Controls.Add(this.picHide);
+            this.Controls.Add(this.chkHide);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnĐK);
+            this.Controls.Add(this.txtMK);
+            this.Controls.Add(this.txtDN);
+            this.Controls.Add(this.btnLogin);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "frmĐN";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng Nhập";
+            this.Load += new System.EventHandler(this.frmĐN_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picHide)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picShow)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtDN;
+        private System.Windows.Forms.TextBox txtMK;
+        private System.Windows.Forms.Button btnĐK;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.CheckBox chkHide;
+        private System.Windows.Forms.PictureBox picHide;
+        private System.Windows.Forms.PictureBox picShow;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using dangki;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace baitap1
+{
+    public partial class frmĐN : Form
+    {
+        public frmĐN()
+        {
+            InitializeComponent();
+        }
+
+        private void btnĐK_Click(object sender, EventArgs e)
+        {
+            frmĐK f = new frmĐK();
+            this.Hide();
+            f.ShowDialog();
+            this.Show();
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtDN.Text == "admin" && txtMK.Text == "admin") // thay admin = tai khoan tren database
+            {
+                MessageBox.Show("Đăng nhập thành công");
+            }
+            else
+            {
+                MessageBox.Show("Sai tài khoản hoặc mật khẩu");
+            }
+        }
+
+        private void txtMK_TextChanged(object sender, EventArgs e)
+        {
+            txtMK.PasswordChar = '*';
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkHide.Checked)
+            {
+                txtMK.PasswordChar = '\0';
+                picShow.Hide();
+                picHide.Show();
+            }
+            else
+            {
+                txtMK.PasswordChar = '*';
+                picShow.Show();
+                picHide.Hide();
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picShow_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmĐN_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}

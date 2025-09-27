224253d5139a87d6ad559577d605202eb662f002
asheryy <24521557@gm.uit.edu.vn>
2025-09-27 20:44:43 +0700

    thay doi 3

---
+++ Formhienthi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _12312312
+{
+    public partial class frmhienThi : Form
+    {
+        public frmhienThi()
+        {
+            InitializeComponent();
+        }
+
+        private void frmhienThi_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTK_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblThongTinTk_Click(object sender, EventArgs e)
+        {
+            // add cai db vao day
+        }
+
+        private void lblThongTinMK_Click(object sender, EventArgs e)
+        {
+            // add db mat khau
+        }
+    }
+}
